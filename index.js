a30ae7ef8670aee88c13914cba748b5891d09f9b
raytchellee <amakaoforkaja@gmail.com>
2022-11-11 16:13:34 +0100

    added an arrow functionality to navigate between images

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+var images = [
+    '/images/Amsterdam.jpg',
+    '/images/China.jpg',
+    '/images/Colombia.jpg',
+    '/images/Japan.jpg',
+    '/images/Oman.jpg',
+    '/images/Thailand.jpg'    
+]
+
+var imgCaption = [
+    'Amsterdam',
+    'China',
+    'Colombia',
+    'Japan',
+    'Oman',
+    'Thailand'    
+]
+
+var index = 0;
+
+
+var imageElem = document.getElementById('image');
+imageElem.setAttribute("src", images[index]);
+var caption = document.getElementById('caption-text');
+
+
 
 
 function hoverImage(){
@@ -11,6 +37,26 @@ function hoverImage(){
         console.log('mouseout')
         getArrowBtn[0].style.display = 'none'
     })
+}
+
+function prev(){
+    if(index == 0){
+        index = images.length -1;
+    }else{
+        index--;
+    }
+    imageElem.setAttribute('src', images[index]);
+    caption.innerHTML = imgCaption[index];
+}
+
+function next(){
+    if(index == images.length -1){
+        index = 0;
+    }else{
+        index++;
+    }
+    imageElem.setAttribute('src', images[index]);
+    caption.innerHTML = imgCaption[index];
 }
 
 hoverImage();
